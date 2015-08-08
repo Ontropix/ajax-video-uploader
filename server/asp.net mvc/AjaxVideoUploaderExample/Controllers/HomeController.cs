3bb030218236201e81ba547ce9c2e0a6685e348c
megaherz <megaherz@bk.ru>
2015-08-08 19:41:06 +0300

    mvc example

---
+++ server/asp.net mvc/AjaxVideoUploaderExample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace AjaxVideoUploaderExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ server/asp.net mvc/AjaxVideoUploaderExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxVideoUploaderExample.MvcApplication" Language="C#" %>
